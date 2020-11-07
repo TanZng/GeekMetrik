f7bbc0a5c07bf286a0395f80e1b97175a8eb5b60
Tan <taniazuniga.mx@gmail.com>
2020-11-07 09:47:05 -0600

    Update Package GUIS Reseñas

--- src/main/java/View/GUIS_Resenias/GUI_EscribirResenia.java
+++ src/main/java/View/GUIS_Resenias/GUI_EscribirResenia.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package View.Resenias;
+package View.GUIS_Resenias;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
