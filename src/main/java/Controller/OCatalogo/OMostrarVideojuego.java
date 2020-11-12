fa1741cbb8be9ced6e6b168d44696c68c9bd9c54
Emiliano Montoya <herobrinee01@gmail.com>
2020-11-12 01:01:56 -0600

    Update OMostrarVideojuego.java
    Se agregó ereferencia a ID del videojuego.

--- src/main/java/Controller/OCatalogo/OMostrarVideojuego.java
+++ src/main/java/Controller/OCatalogo/OMostrarVideojuego.java
@@ -6,10 +6,12 @@ import View.GUIS_Catalogo.GUI_Mostrar_Videojuego;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JLabel;
 
 public class OMostrarVideojuego implements ActionListener {
 
     GUI_Mostrar_Videojuego gui;
+    public static JLabel labelID;
 
     public OMostrarVideojuego(Videojuego vid, GUI_Mostrar_Videojuego gui) {
         this.gui = gui;
@@ -26,6 +28,12 @@ public class OMostrarVideojuego implements ActionListener {
         gui.jLabelTitulo.setText(gui.v.getTitulo());
         gui.jTextAreaDesc.setText(gui.v.getDescripcion());
         gui.jTextFieldEstrellas.setText(String.valueOf(gui.v.getEstrellas()));
+        
+        int ID = gui.v.getId_videojuego();
+        labelID = new JLabel(Integer.toString(ID));
+        labelID.setVisible(false);
+        
+        
     }
 
 }
